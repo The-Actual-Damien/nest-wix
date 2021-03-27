314abeae7d8de4d69d79c50b7d77258eaa460510
ablause <46499306+ablause@users.noreply.github.com>
2021-03-27 20:01:16 +0100

    Add tests e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { EventEmitterModule } from '@nestjs/event-emitter'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 
 import { WixModule } from '../src/module/wix.module'
+import { parse as qsParse } from 'querystring'
+
+const wixConfig = { baseUrl: 'http://localhost:8080/', appId: 'testApp', appSecretKey: '', appPublicKey: '' }
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [WixModule]
+      imports: [WixModule.forRoot(wixConfig), EventEmitterModule.forRoot()]
     }).compile()
 
     app = moduleFixture.createNestApplication()
+
+    app.setGlobalPrefix('/api')
+
     await app.init()
   })
 
-  it('/ (GET)', async () => {
+  it('/signup (GET)', async () => {
+    const token = 'testToken'
+    const redirectUrl = new URL('/api/login', wixConfig.baseUrl)
+
     return await request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!')
+      .get(`/api/signup?token=${token}`)
+      .expect(302)
+      .then((res) => {
+        const url: string = res.header.location
+        const [origin, query] = url.split('?')
+
+        const queries = qsParse(query)
+
+        expect(origin).toBe('https://www.wix.com/installer/install')
+        expect(queries).toEqual({ token, appId: wixConfig.appId, redirectUrl: redirectUrl.href })
+      })
   })
 })
